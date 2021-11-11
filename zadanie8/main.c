d23023b24c94589db04b977c6fc98dc61b9251b9
oskarszysiak <oskszysiak@gmail.com>
2021-11-11 12:35:40 +0100

    3rd commit'

--- zadanie8/main.c
+++ zadanie8/main.c
@@ -3,6 +3,71 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char zdanie[]="KOD CEZARA";
+    int liczbaznakow;
+    liczbaznakow=sizeof(zdanie)/sizeof(char);
+    char zaszyfrowanezdanie[liczbaznakow];
+    char odszyfrowanezdanie[liczbaznakow];
+    const int ileprzesuwamy=1;
+    int t;
+    int u;
+   //szyfrowanie
+    for(int i=0; i<liczbaznakow-1; i++)
+    {
+        if(zdanie[i]==' ')
+        {
+            zaszyfrowanezdanie[i]=' ';
+            continue;
+        }
+        t=zdanie[i]+ileprzesuwamy;
+        if(t<65)
+        {
+            zaszyfrowanezdanie[i]=zdanie[i]+ileprzesuwamy+26;
+        }
+        else if(t>90)
+        {
+            zaszyfrowanezdanie[i]=zdanie[i]+ileprzesuwamy-26;
+        }
+        else
+        zaszyfrowanezdanie[i]=zdanie[i]+ileprzesuwamy;
+    }
+
+
+
+    printf("zaszyfrowane zdanie:\n");
+    for(int i=0; i<liczbaznakow-1; i++)
+    {
+        printf("%c",zaszyfrowanezdanie[i]);
+    }
+    printf("\n");
+
+
+
+    //deszyfrowanie
+    for(int i=0; i<liczbaznakow-1; i++)
+    {
+        if(zaszyfrowanezdanie[i]==' ')
+        {
+            odszyfrowanezdanie[i]=' ';
+            continue;
+        }
+        u=zaszyfrowanezdanie[i]-ileprzesuwamy;
+        if(u<65)
+        {
+            odszyfrowanezdanie[i]=zaszyfrowanezdanie[i]-ileprzesuwamy+26;
+        }
+        else if(u>90)
+        {
+            odszyfrowanezdanie[i]=zaszyfrowanezdanie[i]-ileprzesuwamy-26;
+        }
+        else
+        odszyfrowanezdanie[i]=zaszyfrowanezdanie[i]-ileprzesuwamy;
+    }
+
+
+    printf("odszyfrowane zdanie:\n");
+    for(int i=0; i<liczbaznakow-1; i++)
+    {
+        printf("%c",odszyfrowanezdanie[i]);
+    }
 }
