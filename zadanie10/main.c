5954df946308d00f89721b0ffb9e44f4fce4f777
oskarszysiak <93039256+oskarszysiak@users.noreply.github.com>
2021-11-12 23:26:53 +0100

    4th commit

--- zadanie10/main.c
+++ zadanie10/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+        int l[10];
+        srand(time(NULL));
+        for(int i=0;i<10;i++)
+        {
+            l[i]=rand()%100;
+        }
+
+        for(int i=0;i<10;i++)
+        {
+            printf("%d\t",l[i]);
+        }
+
+
+        printf("\n");
+
+
+        int przechowalnia;
+        for(int i=0; i<10;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                if(l[j]>l[j+1])
+                {
+                    przechowalnia=l[j];
+                    l[j]=l[j+1];
+                    l[j+1]=przechowalnia;
+                }
+            }
+        }
+
+        for(int i=0;i<10;i++)
+        {
+            printf("%d\t",l[i]);
+        }
+        return 0;
 }
